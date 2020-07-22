8aef60368aec0fd6690b171040541fa1aace937e
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2020-07-22 20:49:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -54,4 +54,4 @@ const {name, color, song, job, partner} = muppet;
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
 
-const {song_2, song_4} = {job, partner};
+const {song_2, song_4, k_job, k_partner} = 
